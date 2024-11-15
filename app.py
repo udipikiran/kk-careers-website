f21933aa58a960f9ceec14ef9f98b3eee8103e29
udipikiran <udupikirankumar@gmail.com>
2024-11-15 17:53:58 +0530

    add html and css content

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder='static', static_url_path="/")
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return render_template("home.html")
 
 print(__name__)
 if __name__ == "__main__":
