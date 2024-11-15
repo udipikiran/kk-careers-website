8db00e0c9b32412af87a036d0e8fe0cc356463a4
udipikiran <udupikirankumar@gmail.com>
2024-11-15 19:23:21 +0530

    Updated for jobs details to get in dynamic way and rest route

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, template_folder="templates", static_folder='static', static_url_path="/")
 
+JOBS = [
+    {
+        'id':1,
+        'title':'Data Analayst',
+        'location':'Bangalore',
+        'salary':'10L INR'
+    },
+    {
+        'id':2,
+        'title':'Data Scientist',
+        'location':'Bangalore',
+        'salary':'20L INR'
+    },
+    {
+        'id':3,
+        'title':'DBA',
+        'location':'Bangalore',
+        'salary':'15L INR'
+    },
+    {
+        'id':4,
+        'title':'Middleware',
+        'location':'Bangalore',
+        'salary':'18L INR'
+    }
+]
+
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("home.html", jobs = JOBS)
+
+@app.route("/jobs")
+def listjobs():
+    return jsonify(JOBS)
 
 print(__name__)
 if __name__ == "__main__":
